1a4f7c22dbb765c7a874dcaa748506c0403446ba
m0x0m0x <mx0mx@tuta.io>
2024-03-04 10:35:19 +0530

    chk python

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python3
 # Run `python main.py` in the terminal
 
 # Note: Python is lazy loaded so the first run will take a moment,
 # But after cached, subsequent loads are super fast! ⚡️
 
 import platform
+import p1 as p 
+
 print(f"Hello Python v{platform.python_version()}!")
+p.myPussy() 
---
+++ p1.py
@@ -0,0 +1,6 @@
+# Testing multi file linking 
+
+def myPussy():
+    print(f"""
+Testing out p1 pussy
+""")
